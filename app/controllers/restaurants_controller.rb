1150d9cdccf2f4c7a62f9329ae3cc62765693c2d
felixottander <felix.ottander@yahoo.com>
2017-03-28 21:10:47 +0200

    Added restaurant model. Modified the restaurant routes. Added the form for creating the restaurant in the view.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,21 @@
 class RestaurantsController < ApplicationController
-  def new
 
+  def new
+    @restaurants = Restaurant.new
+  end
+
+  def create
+    @restaurants = Restaurant.new restaurant_params
+
+    if @restaurants.save
+      flash[:notice] = "Restaurant successfully created"
+      redirect_to new_restaurant_path
+    end
+  end
+
+  private
+
+  def restaurant_params
+   params.require(:restaurant).permit(:name, :street, :zip_code, :city, :food_style)
   end
 end
--- features/step_definitions/basic_steps.rb
+++ features/step_definitions/basic_steps.rb
@@ -2,10 +2,10 @@ Given(/^I am on the restaurant registration page$/) do
   visit new_restaurant_path
 end
 
-When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |arg1, arg2|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I fill in "([^"]*)" with "([^"]*)"$/) do |field, content|
+  fill_in field, with: content
 end
 
-When(/^I click "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
+When(/^I click "([^"]*)"$/) do |button|
+  click_link_or_button button
 end
