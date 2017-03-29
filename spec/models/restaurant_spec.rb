7e2ca7ee9e5346c65a1ff564f543c2dc05712e79
felixottander <felix.ottander@yahoo.com>
2017-03-29 10:24:55 +0200

    Adds validations to the rstaurant db.

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -9,4 +9,12 @@ RSpec.describe Restaurant, type: :model do
     it { is_expected.to have_db_column :food_style }
   end
 
+  describe 'Validations' do
+    it { is_expected.to validate_presence_of :name }
+    it { is_expected.to validate_presence_of :street }
+    it { is_expected.to validate_presence_of :zip_code }
+    it { is_expected.to validate_presence_of :city }
+    it { is_expected.to validate_presence_of :food_style }
+  end
+
 end
