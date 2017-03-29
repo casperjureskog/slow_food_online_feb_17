997b214b4e9fc5795195999c449a3a6a8e5c41c5
felixottander <felix.ottander@yahoo.com>
2017-03-29 16:53:13 +0200

    Added the method to find the restaurant´s menu name and display it in the view.

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,6 +2,7 @@ class MenusController < ApplicationController
 
   def show
     @restaurant = Restaurant.find(params[:restaurant_id])
+    @menu = Menu.find_by(restaurant: @restaurant)
   end
 
 end
