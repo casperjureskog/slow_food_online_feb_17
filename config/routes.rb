4d8c2b58be51f295bb2c0a94b0654fd1cf23fe0f
casperjureskog <lcasper@mac.com>
2017-03-30 22:05:47 +0200

    add routes for cart and modefide view for menu

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
   root controller: :index, action: :index
 
+  resources :shopping_carts, only: [:show], as: :cart do
+    resources :shopping_cart_items, only: [:create], as: :items
+  end
+
   resources :restaurants, only: [:show, :new, :create] do
     resources :menus, only: [:show]
   end
