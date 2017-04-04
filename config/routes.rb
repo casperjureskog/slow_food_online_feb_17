f82a469e97202e3ab58795a81178d634f0436491
johan <johan.schantz@telia.com>
2017-04-04 11:42:16 +0200

    fixed route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, only: [:registration]
+  devise_for :users
   root controller: :index, action: :index
   post '/', controller: :index, action: :select_category, as: :select_category
 
