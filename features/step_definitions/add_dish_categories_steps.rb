3a5cee77fc3b76bf9c052e76c8041d9e927f7b43
casperjureskog <lcasper@mac.com>
2017-04-05 17:07:11 +0200

    add rspec and cucumber test for add category

---
+++ features/step_definitions/add_dish_categories_steps.rb
@@ -0,0 +1,7 @@
+Given(/^I am on the dishes show$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^I should see "([^"]*)" in "([^"]*)"$/) do |arg1, arg2|
+  pending # Write code here that turns the phrase above into concrete actions
+end
