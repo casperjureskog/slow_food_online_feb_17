4c59d0a146e2f343228a95a8596a51810fb51f56
casperjureskog <lcasper@mac.com>
2017-04-12 13:26:02 +0200

    add request spec and routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :restaurants, only: [:index], defaults: { format: 'json' }
+      resources :restaurants, only: [:show], defaults: { format: 'json' }
     end
   end
 end
