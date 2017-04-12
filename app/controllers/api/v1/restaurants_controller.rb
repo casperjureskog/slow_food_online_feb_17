ac23dc082e94146529d5b181293997bab9e76ea9
johan <johan.schantz@telia.com>
2017-04-12 13:38:25 +0200

    added view and controller to resturant show

--- app/controllers/api/v1/restaurants_controller.rb
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -2,4 +2,8 @@ class Api::V1::RestaurantsController < ApplicationController
   def index
     @restaurants = Restaurant.all
   end
+
+  def show
+    @restaurants = Restaurant.find(params[:id])
+  end
 end
---
+++ app/views/api/v1/restaurants/show.json.jbuilder
@@ -0,0 +1,7 @@
+json entries
+  json.name  menu.menu.name
+  json.food_
+  json.name dish.name
+  json.price dish.price
+  json.category dish.category
+  json.description dish.description
